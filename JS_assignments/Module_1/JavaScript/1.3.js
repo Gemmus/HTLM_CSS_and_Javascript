783b83cef597c93042017553636d4169499907a8
Gemmus <decadentcrown@gmail.com>
2022-11-08 18:43:45 +0200

    Changing the method of conversion.

--- JS_assignments/Module_1/JavaScript/1.3.js
+++ JS_assignments/Module_1/JavaScript/1.3.js
@@ -3,9 +3,9 @@ const number_1 = prompt('Please enter an integer: ');
 const number_2 = prompt('Please enter an integer: ');
 const number_3 = prompt('Please enter an integer: ');
 
-const num1 = parseInt(number_1);
-const num2= parseInt(number_2);
-const num3 = parseInt(number_3);
+const num1 = +number_1;
+const num2= +number_2;
+const num3 = +number_3;
 
 let summary = num1 + num2 + num3
 let product = num1 * num2 * num3
