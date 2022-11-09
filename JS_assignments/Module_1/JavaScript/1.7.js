79f88d1696ec49874990e3d9ba7354b853e39c5d
Gemmus <decadentcrown@gmail.com>
2022-11-09 10:27:24 +0200

    Initial upload.

---
+++ JS_assignments/Module_1/JavaScript/1.7.js
@@ -0,0 +1,16 @@
+'use strict';
+let numberOfDices = prompt('Please enter the number of dices: ');
+
+while (numberOfDices < 0) {
+    numberOfDices = prompt('Please enter the valid number of dices: ');
+}
+
+let number = parseInt(numberOfDices);
+let summary = 0;
+for ( let n = 0; n < number; n++) {
+    let rolledNumber = Math.floor(Math.random()*6)+1;
+    console.log(rolledNumber)
+    summary += rolledNumber
+}
+
+document.querySelector('#sumOfDices').innerHTML = 'The added value of the dices: ' + summary + '';
