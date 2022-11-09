54190d319b447a21fc195d98b9b7b5d8d337bbf1
Gemmus <decadentcrown@gmail.com>
2022-11-09 15:08:45 +0200

    Modification of if conditions.

--- JS_assignments/Module_1/JavaScript/1.5.js
+++ JS_assignments/Module_1/JavaScript/1.5.js
@@ -4,19 +4,13 @@ while (year < 0) {
     year = prompt('Please enter a valid year:');
 }
 let Year = parseInt(year);
-if ((Year %= 4) === 0) {
-    if ((Year %= 100) !== 0) {
-            document.querySelector('#result').innerHTML = '' + year + ' is a year.';
-    }
-    else {
-        if ((Year %= 400) === 0) {
+if ((Year % 4) === 0)
+    if ((Year % 100) !== 0)
+            document.querySelector('#result').innerHTML = '' + year + ' is leap a year.';
+    else
+        if ((Year % 400) === 0)
             document.querySelector('#result').innerHTML = '' + year + ' is leap year.';
-        }
-        else {
+        else
             document.querySelector('#result').innerHTML = '' + year + ' is not a leap year.';
-        }
-    }
-}
-else {
+else
     document.querySelector('#result').innerHTML = '' + year + ' is not a leap year.';
-}
