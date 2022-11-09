84b8826ef57feecc8b3a96fffb6f009d41358352
Gemmus <decadentcrown@gmail.com>
2022-11-09 16:55:01 +0200

    Adjustment.

--- JS_assignments/Module_2/JavaScript/2.1.js
+++ JS_assignments/Module_2/JavaScript/2.1.js
@@ -4,8 +4,7 @@ const number_2 = prompt('Please enter a number:');
 const number_3 = prompt('Please enter a number:');
 const number_4 = prompt('Please enter a number:');
 const number_5 = prompt('Please enter a number:');
-let list_of_number = [];
-list_of_number = [number_1, number_2, number_3, number_4, number_5];
+let list_of_number = [number_1, number_2, number_3, number_4, number_5];
 
 for (let i = list_of_number.length - 1; i >= 0; i--) {
         console.log(`Number: ${list_of_number[i]}`);
