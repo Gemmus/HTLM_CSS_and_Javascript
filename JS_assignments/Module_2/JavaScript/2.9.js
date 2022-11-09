35d097d84e2c76af5a51442e941a67add570635e
Gemmus <decadentcrown@gmail.com>
2022-11-09 20:09:10 +0200

    Initial upload.

---
+++ JS_assignments/Module_2/JavaScript/2.9.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function even(numbers) {
+    let even_numbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if ((numbers[i] % 2) === 0) {
+            even_numbers.push(numbers[i]);
+        }
+    }
+    return even_numbers;
+}
+
+let old_list = [34, 57, 79, 26, 90, 43, 8, 13, 61, 88, 102];
+let new_list = even(old_list);
+console.log(old_list);
+console.log(new_list);
