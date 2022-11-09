77faa276098c09d9da675f2f3e5b70bdd795b56b
Gemmus <decadentcrown@gmail.com>
2022-11-09 10:59:57 +0200

    Initial upload.

---
+++ JS_assignments/Module_1/JavaScript/1.6.js
@@ -0,0 +1,17 @@
+'use strict';
+let inquiry = confirm('Should I calculate the square root?');
+
+if (inquiry === true) {
+    let enteredNumber = prompt('Please enter a number:');
+    if (enteredNumber < 0) {
+        document.querySelector('#outcome').innerHTML = 'The square root of a negative number is not defined.';
+    }
+    else {
+        let number = parseInt(enteredNumber)
+        let squareRoot = number * number
+        document.querySelector('#outcome').innerHTML = 'The square root of the number is: ' + squareRoot + '.';
+    }
+}
+else {
+    document.querySelector('#outcome').innerHTML = 'The square root is not calculated.';
+}
