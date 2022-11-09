22b1cd4f448b1e2168cfacb72d02d2bfc6deb539
Gemmus <decadentcrown@gmail.com>
2022-11-09 15:08:26 +0200

    Initial upload.

---
+++ JS_assignments/Module_1/JavaScript/1.8.js
@@ -0,0 +1,25 @@
+'use strict';
+let startYear = prompt('Please enter the starting year:');
+let endYear = prompt('Please enter the ending year:');
+let start = parseInt(startYear);
+let end = parseInt(endYear);
+
+let list = []
+while (start <= end) {
+    if ((start % 4) === 0)
+        if ((start % 100) !== 0)
+            list.push(start);
+    else
+        if ((start % 400) === 0)
+            list.push(start);
+    start++;
+}
+
+let ul = document.createElement('ul');
+
+for (let l of list) {
+    let li = document.createElement('li');
+    li.innerText = l;
+    ul.append(li);
+}
+document.body.append(ul);
