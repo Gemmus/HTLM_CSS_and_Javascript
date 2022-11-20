a9da01ef43bc7dfa0d5c42d216520e6dacb2cb41
Gemmus <decadentcrown@gmail.com>
2022-11-20 14:51:55 +0200

    Initial upload.

--- game-starter-HTML-main/js/main.js
+++ game-starter-HTML-main/js/main.js
@@ -9,35 +9,138 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 map.setView([60, 24], 7);
 
 
-// global variables
+// 11. global variables
+const apiUrl = 'http://127.0.0.1:5000/';
+const startLoc = 'EFHK';
+const globalGoals = [];
+const airportMarkers = L.featureGroup().addTo(map);
 
-// icons
+// 3. icons
+const blueIcon = L.divIcon({className: 'blue-icon'});
+const greenIcon = L.divIcon({className: 'green-icon'});
 
-// form for player name
+// 10. form for player name
+document.querySelector('#player-form').addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  const playerName = document.querySelector('#player-input').value;
+  document.querySelector('#player-modal').classList.add('hide');
+  gameSetup(`${apiUrl}newgame?player=${playerName}&loc=${startLoc}`);
+    });
 
-// function to fetch data from API
+// 2. function to fetch data from API
+async function getData(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Invalid server input!');
+  const data = await response.json();
+  return data
+}
 
-// function to update game status
+// 4. function to update game status
+function updateStatus(status) {
+  document.querySelector('#player-name').innerHTML = `Player: ${status.name}`;
+  document.querySelector('#consumed').innerHTML = status.co2.consumed;
+  document.querySelector('#budget').innerHTML = status.co2.budget;
+}
 
-// function to show weather at selected airport
+// 5. function to show weather at selected airport
+function showWeather(airport) {
+  document.querySelector('#airport-name').innerHTML = `Weather at ${airport.name}`;
+  document.querySelector('#airport-temp').innerHTML = `${airport.weather.temp}°C`;
+  document.querySelector('#weather-icon').src= airport.weather.icon;
+  document.querySelector('#airport-conditions').innerHTML = airport.weather.description;
+  document.querySelector('#airport-wind').innerHTML = `${airport.weather.wind.speed}m/s`;
+}
 
-// function to check if any goals have been reached
+// 7. function to check if any goals have been reached
+function checkGoals(meets_goals) {
+  if (meets_goals.length > 0) {
+    for (let goal of meets_goals) {
+      if (!globalGoals.includes(goal)) {
+            document.querySelector('.goal').classList.remove('hide');
+    location.href = `#goals`;
+      }
+    }
+  }
+}
 
-// function to update goal data and goal table in UI
+// 6. function to update goal data and goal table in UI
+function updateGoals(goals) {
+  document.querySelector('#goals').innerHTML = '';
+  for (let goal of goals) {
+    const li = document.createElement('li');
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    const figcaption = document.createElement('figcaption');
+    img.src = goal.icon;
+    img.alt = `goal name: ${goal.name}`;
+    figcaption.innerHTML = goal.description;
+    figure.append(img);
+    figure.append(figcaption);
+    li.append(figure);
+    if (goal.reached) {
+      li.classList.add('done');
+      globalGoals.includes(goal.goalid) || globalGoals.push(goal.goalid);
+    }
+    document.querySelector('#goals').append(li);
+  }
+}
 
-// function to check if game is over
+// 9. function to check if game is over
+function checkGameOver(budget) {
+  if (budget < 0) {
+    alert(`Game Over. ${globalGoals.length} goals reached.`);
+    return false;
+  }
+  return true;
+}
 
 // function to set up game
-// this is the main function that creates the game and calls the other functions
-async function gameSetup() {
+// 1. this is the main function that creates the game and calls the other functions
+async function gameSetup(url) {
   try {
-    const response = await fetch('testdata/newgame.json');
-    const data = await response.json();
-    console.log(data);
+    document.querySelector('.goal').classList.add('hide');
+    airportMarkers.clearLayers();
+    const gameData = await getData(url);
+    console.log(gameData);
+    updateStatus(gameData.status);
+    if(!checkGameOver(gameData.status.co2.budget)) return;
+
+    for (let airport of gameData.location) {
+      const marker = L.marker([airport.latitude, airport.longitude]).addTo(map);
+      airportMarkers.addLayer(marker);
+      if(airport.active) {
+              map.flyTo([airport.latitude, airport.longitude], 10);
+              showWeather(airport);
+              checkGoals(airport.weather.meets_goals);
+              marker.bindPopup(`You are here: <b>${airport.name}</b>`);
+              marker.openPopup();
+              marker.setIcon(greenIcon);
+      } else {
+        marker.setIcon(blueIcon);
+        const popupContent = document.createElement('div');
+        const h4 = document.createElement('h4');
+        h4.innerHTML = airport.name;
+        popupContent.append(h4);
+        const goButton = document.createElement('button');
+        goButton.classList.add('button');
+        goButton.innerHTML = 'Fly here';
+        popupContent.append(goButton);
+        const p = document.createElement('p');
+        p.innerHTML = `Distance ${airport.distance}km`;
+        popupContent.append(p);
+        marker.bindPopup(popupContent);
+        goButton.addEventListener('click', function () {
+          gameSetup(`${apiUrl}flyto?game=${gameData.status.id}&dest=${airport.ident}&consumption=${airport.co2_consumption}`);
+        })
+      }
+    }
   } catch (error) {
     console.log(error);
   }
 }
+updateGoals(gameData.goals);
 
-gameSetup();
-// event listener to hide goal splash
+// 8. event listener to hide goal splash
+document.querySelector('.goal').addEventListener('click', function (evt){
+  evt.currentTarget.classList.add('hide');
+})
