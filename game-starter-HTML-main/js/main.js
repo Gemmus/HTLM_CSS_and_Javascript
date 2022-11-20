cc52e83ecca7ce1a1ab84ab6a59db33008b458d0
Gemmus <decadentcrown@gmail.com>
2022-11-20 09:55:14 +0200

    Initial upload.

--- game-starter-HTML-main/js/main.js
+++ game-starter-HTML-main/js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 /* 1. show map using Leaflet library. (L comes from the Leaflet library) */
-/*
+
 const map = L.map('map', {tap: false});
 L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   maxZoom: 20,
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
 map.setView([60, 24], 7);
- */
+
 
 // global variables
 
@@ -29,5 +29,15 @@ map.setView([60, 24], 7);
 
 // function to set up game
 // this is the main function that creates the game and calls the other functions
+async function gameSetup() {
+  try {
+    const response = await fetch('testdata/newgame.json');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+gameSetup();
 // event listener to hide goal splash
